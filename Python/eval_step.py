6d4ed058a0f49e9e184163b608a73ba12a124c6a
Jae-Won Sa <92181151+Jaewon-Sa@users.noreply.github.com>
2023-09-18 16:57:35 +0900

    Add evaluations
    Add evaluations and logs for validation datasets in the learning phase
    Speed up model Performance

--- Python/eval_step.py
+++ Python/eval_step.py
@@ -14,6 +14,7 @@ from defaultbox import default
 from Detect import Detect
 
 from matplotlib import pyplot as plt
+import time
 
 def get_iou(a,b):
     if len(a)!=4 or len(b)!=4:
@@ -148,7 +149,6 @@ def test_step(model, Data_loader, image_size=(300,300), device="cpu"):
     total_Precison = 0
     N=0
     for idx, data in enumerate(Data_loader):
-          
         images = data[0].to(device)
         labels = [label.cpu() for label in data[1]] # batch, 객체개수, 5
         
@@ -158,18 +158,23 @@ def test_step(model, Data_loader, image_size=(300,300), device="cpu"):
 
         with torch.no_grad():
             cls, loc = model(images)
-            #cls = (batch,ddobx,3)
+            
+            ''' 
+            #For Debug
+            cls = (batch,ddobx,3)
             value, i= torch.max(cls, dim=-1)
             s1 = i == 1
             s2 = i == 2
             bg = i == 0
-            #상위 200개에 대한 detection
-            #size=(batch, numclass ,200, 5)
+            상위 200개에 대한 detection
+            size=(batch, numclass ,200, 5)
+            '''
+            
+
             output = detect.forward(loc, cls, tensor_d, num_classes = cls.size(-1),  bkg_label=0, top_k=200, conf_thresh=0.4, nms_thresh=0.5)
         all_boxes = [[[] for _ in range(output.size(0))]
                  for _ in range(output.size(1))]  #all_boxes[cls][image]
-        
-        
+
         for i in range(output.size(0)):
             for j in range(1, output.size(1)):#class 종류
                 dets = output[i, j, :]
@@ -223,7 +228,7 @@ def test_step(model, Data_loader, image_size=(300,300), device="cpu"):
                     TPFP_filter.append(pred_det[:2])
                     
             TPFP_cls_filter[cls_idx] = TPFP_cls_list
-        
+
         #print(TPFP)
         #print(TPFN)
         #print(total_TP_1,total_TP_2)
@@ -233,7 +238,7 @@ def test_step(model, Data_loader, image_size=(300,300), device="cpu"):
         Recall = total_TP_1 / (total_TP_1 + total_FN) #total_labels
         total_Recall += Recall
         total_Precison += Precison
-        
+
         ap +=  get_ap(total_TP_1, total_FP, total_FN, TPFP_filter)
         N = idx+1
         
--- Python/train_step.py
+++ Python/train_step.py
@@ -10,10 +10,10 @@ from torch import nn
 from torch import optim
 from defaultbox import default
 from loss import MultiBoxLoss
-
+from eval_step import *
 DIR_PATH = "./objectdetection_model"     
 
-def train_step(model, Data_loader, epoch_num, batchsize, 
+def train_step(model, test_model, train_Data_loader, valid_Data_loader, epoch_num, batchsize, 
                optim_type="SGD", lr=2e-3, device="cpu", model_name="BASE", 
                is_wandb=False, is_freeze=True):
     
@@ -50,20 +50,21 @@ def train_step(model, Data_loader, epoch_num, batchsize,
     
     epoch_train_loss = 0.0 # 에포크 손실 합
     #epoch_val_loss = 0.0
-    model.to(device)
+    
     for epoch in range(epoch_num):
         epoch_start = time.time()
         iter_start = time.time()
         print("Epoch : {0} / {1}".format(epoch+1,epoch_num))
         
         #targets=np.array([[[1,2,3,4,5],[1,2,3,4,5]],[[1,2,3,4,5],[1,2,3,4,5]]])
-        for idx, data in enumerate(Data_loader):
+        for idx, data in enumerate(train_Data_loader):
             
             #img=torch.zeros((32,3,300,300),dtype=torch.float)
-
+            model.to(device)
             images = data[0].to(device)
-
+            
             labels = [label.to(device) for label in data[1]]
+            
             optimizer.zero_grad()#이전 값 들에 대한 가중치 기울기 초기화
             with torch.set_grad_enabled(True):
                 cls, loc = model(images)
@@ -80,13 +81,8 @@ def train_step(model, Data_loader, epoch_num, batchsize,
             if(idx % 50 == 0):
                 iter_end = time.time()
                 
-                value, i = torch.max(cls, dim=-1)
-                s1 = i == 1
-                s2 = i == 2
-                bg = i == 0
-                print(s1.sum(),s2.sum(),bg.sum())
                 print("Current Batch {0} / {1} | Cls Loss : {2:.3f}, Loc Loss : {3:.3f}, Total Loss : {4:.3f} | 50 iter time {5:.4f}: "
-                      .format(idx, len(Data_loader), loss_l.item(), loss_c.item(), loss.item(), iter_end - iter_start))
+                      .format(idx, len(train_Data_loader), loss_l.item(), loss_c.item(), loss.item(), iter_end - iter_start))
                 
                 if is_wandb==True:
                     wandb.log({"total_loss": loss.item(),
@@ -97,9 +93,27 @@ def train_step(model, Data_loader, epoch_num, batchsize,
                 
             epoch_train_loss+=loss.item()
             
-        epoch_end = time.time()        
-        print("Epoch : {0} / {1} of Total Loss : Total Loss : {2:.3f} | 1 epoch update time : {3:.2f}s"
-              .format(epoch+1, epoch_num, epoch_train_loss,epoch_end-epoch_start))
+        epoch_end = time.time() 
+        
+        if (epoch + 1) % 3 == 0 and  (epoch + 1) >= 15:
+            train_parameters = model.state_dict()
+            test_model.load_state_dict(train_parameters)
+            test_model.eval()
+            
+            torch.cuda.empty_cache()
+            
+            eval_start = time.time()
+            mRecall, mPrecison, mAP = test_step(test_model, valid_Data_loader, device = device)
+
+            eval_end = time.time()
+            print((f'Epoch : {epoch+1} / {epoch_num} | Total Loss : {epoch_train_loss:.3f}' 
+                   f'| 1 epoch update time : {epoch_end-epoch_start:.2f}s |' 
+                   f'mRecall : {mRecall:.2%} , mPrecison : {mPrecison:.2%}, mAP: {mAP:.3f}, eval_time : {eval_end - eval_start:.2f}s'))
+            torch.cuda.empty_cache()
+            
+        else:
+            print("Epoch : {0} / {1} of Total Loss : Total Loss : {2:.3f} | 1 epoch update time : {3:.2f}s"
+                  .format(epoch+1, epoch_num, epoch_train_loss,epoch_end-epoch_start))
         print("-----------------------------------------------")
         epoch_train_loss=0
         
